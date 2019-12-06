83f6541f48fd65ea5abefa71e37d1437bfc71433
UliKuch <u.kuchelmeister@gmx.de>
2019-12-06 11:31:56 +0100

    Solve day 6, part 2; clarify part 1

--- day06.js
+++ day06.js
@@ -11,6 +11,21 @@ E)J
 J)K
 K)L`
 
+const testInput2 = `COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN`
+
+
 function calcAllOrbits(input) {
   let c = 0; // counter
 
@@ -30,22 +45,92 @@ function calcAllOrbits(input) {
   return c;
 }
 
-function calcOrbitsOfObject(str, array) {
+
+// calculate all orbits of a given "object"
+function calcOrbitsOfObject(spaceObject, array) {
   let c = 0; // counter
   let match = ""
   
-  // check if input "object" (str) orbits another object (i.e. is at the array[i][1] position)
+  // check if input "object" (spaceObject) orbits another "object" (i.e. is at the array[i][1] position)
   for (let i = 0; i < array.length; i++) {
-    if (array[i][1] === str) {
+    if (array[i][1] === spaceObject) {
       c++;
       match = array[i][0];
       break;
     }
   }
   
-  // if str orbits another "object", add the orbit of that "object" to the return value
+  // if spaceObject orbits another "object", add the orbit of that "object" to the return value
   return (c > 0) ? c + calcOrbitsOfObject(match, array) : 0;
 };
 
+
+function calcDistanceToSanta(input) {
+
+  // split input string into array of tuples
+  const splitArr = input.split("\n")
+  .map(str => {
+    let splitString = str.split(")");
+    return splitString;
+  });
+
+  let you;
+  let santa;
+
+  // find you and santa
+  for (let i = 0; i < splitArr.length; i++) {
+    (splitArr[i][1] === "YOU") ? you = splitArr[i] : null;
+    (splitArr[i][1] === "SAN") ? santa = splitArr[i] : null;
+
+    if ((santa != undefined) && (you != undefined)) {
+      break;
+    }
+  }
+
+  // calculate your path
+  const yourPath = calcPath(you[0], splitArr);
+  const santasPath = calcPath(santa[0], splitArr);
+
+  let commonNode;
+  let yourDistanceToCommonNode;
+  let santasDistanceToCommonNode;
+
+  // find common node and calculate distance to it
+  for (let i = 0; i < yourPath.length; i++) {
+    commonNode = santasPath.find(node => node === yourPath[i]);
+
+    if (commonNode != undefined) {
+      yourDistanceToCommonNode = i + 1;
+      santasDistanceToCommonNode = santasPath.indexOf(commonNode) + 1;
+      break;
+    }
+  }
+
+  return yourDistanceToCommonNode + santasDistanceToCommonNode
+}
+
+
+// calculate whole path of an input "object" (spaceObject)
+function calcPath(spaceObject, array) {
+  let match;
+  let path = [];
+  
+  // check if input "object" (spaceObject) orbits another "object" (i.e. is at the array[i][1] position)
+  // and add that "object" to path
+  for (let i = 0; i < array.length; i++) {
+    if (array[i][1] === spaceObject) {
+      match = array[i][0];
+      path.push(match)
+      break;
+    }
+  }
+  
+  // if spaceObject orbits another "object", add the path of that "object" to the return array (path)
+  return (match != undefined) ? path.concat(calcPath(match, array)) : path;
+};
+
+
 console.log(calcAllOrbits(testInput));
 console.log(calcAllOrbits(input));
+console.log(calcDistanceToSanta(testInput2))
+console.log(calcDistanceToSanta(input));
