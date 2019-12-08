6aea0d2c3eb4647569173f49b9d92a0f10020de0
UliKuch <u.kuchelmeister@gmx.de>
2019-12-08 15:38:09 +0100

    Solve day 8, part 2

--- day08.js
+++ day08.js
@@ -1,6 +1,6 @@
 const input = require("./day08_input");
 
-function findLayer(input, width, height) {
+function findLayerWithLeastZeroes(input, width, height) {
 
   const pixelsPerLayer = width * height;
 
@@ -15,9 +15,43 @@ function findLayer(input, width, height) {
 
   const layerWithLeastZeroes = layers[zerosPerLayer.indexOf(Math.min(...zerosPerLayer))];
 
-
   return layerWithLeastZeroes.match(/1/g).length * layerWithLeastZeroes.match(/2/g).length;
-
 };
 
-console.log(findLayer(input, 25, 6))
+
+function decodeText(input, width, height) {
+
+  const pixelsPerLayer = width * height;
+
+  let layers = [];
+
+  for (let i = 0; i < input.length; i += pixelsPerLayer) {
+
+    layers.push(input.substring(i, i + pixelsPerLayer))
+  }
+
+  let message = "";
+
+  // iterate over every pixel of image
+  for (let i = 0; i < pixelsPerLayer; i++) {
+    // iterate over every layer until there is a non-transparent pixel in the current position (i)
+    // add that pixel to message
+    for (let j = 0; j < layers.length; j++) {
+      if (layers[j][i] < 2) {
+        message += layers[j][i];
+        break;
+      }
+    }
+  }
+
+  // split message into substrings according to width/height properties to display it as an ascii image
+  let messageSplit = [];
+  for (let i = 0; i < message.length; i += width) {
+    messageSplit.push(message.substring(i, i + width))
+  }
+
+  return messageSplit
+}
+
+console.log(findLayerWithLeastZeroes(input, 25, 6))
+console.log(decodeText(input, 25, 6))
