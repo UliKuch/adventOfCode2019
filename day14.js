e3810d9e86ce075281797381ba30ab6f4ff1cc7c
UliKuch <u.kuchelmeister@gmx.de>
2019-12-16 11:06:57 +0100

    Solve day 14, part 2

--- day14.js
+++ day14.js
@@ -7,6 +7,15 @@ const testInput1 = `9 ORE => 2 A
 4 C, 1 A => 1 CA
 2 AB, 3 BC, 4 CA => 1 FUEL`;
 
+const testInput2 = `157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT`;
 
 class Resourse {
   constructor(recipe, name) {
@@ -29,13 +38,15 @@ class Resourse {
     // substract number of required resources from this.existing
     this.existing -= num;
 
-    // produce resource invoking recipe (this.requires) until this.existing is larger or equal to 0
-    while (this.existing < 0) {
+    if (this.existing < 0) {
+
+      // calculate how many times this resource has to be produced
+      let missing = Math.ceil(Math.abs(this.existing) / this.produces);
 
       // iterate over recipe (this.requires)
       for (let [key, value] of Object.entries(this.requires)) {
         if (key === "ORE") {
-          c += value
+          c += missing * value
           continue;
         }
 
@@ -43,10 +54,11 @@ class Resourse {
         resource = resources.find(res => res.name === key)
 
         // increase c by the value returned from requiring the resource as often as specified in recipe (value)
-        c += resource.requiring(value, resources)
-      }
+        c += resource.requiring(missing * value, resources)
+      }	
+      
+      this.existing += missing * this.produces;
 
-      this.existing += this.produces;
     }
     return c;
   }
@@ -72,6 +84,29 @@ function createResourceObjects(input) {
 };
 
 
+function spendOre(num, resources) {
+  let oreUsed = 0;
+  let fuelProduced = 0;
+
+  // find fuel resource object
+  const fuel = resources.find(res => res.name === "FUEL");
+
+  // spend ore, starting with large steps (have to be adjusted for higher num value and for fuel-intensive input values)
+  while (oreUsed < num) {
+    fuelProduced += (oreUsed < 950000000000) ? 10000 : (oreUsed < 990000000000) ? 1000 : 1;
+    oreUsed += (oreUsed < 950000000000)
+        ? fuel.requiring(10000, resources)
+        : (oreUsed < 990000000000)
+        ? fuel.requiring(1000, resources)
+        : fuel.requiring(1, resources);
+  }
+
+  console.log("oreUsed", oreUsed, "fuelProduced", fuelProduced);
+  return fuelProduced - 1;
+}
+
+
 const resources = createResourceObjects(input);
-const fuel = resources.find(res => res.name === "FUEL");
-console.log(fuel.requiring(1, resources))
+// const fuel = resources.find(res => res.name === "FUEL");
+// console.log(fuel.requiring(1, resources))
+console.log(spendOre(1000000000000, resources));
